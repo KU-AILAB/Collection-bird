37830a35b40a21e131cebe6d5387d392ad5d87b7
mini <mini@example.com>
2025-04-15 10:15:33 +0900

    임시

--- client/src/layout/Header/Header.jsx
+++ client/src/layout/Header/Header.jsx
@@ -1,32 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { Link, useNavigate } from "react-router-dom";
-import "./../../style/Header.css"
-
-const Header = () => {
-  const navigate = useNavigate();
-
-  const navigateToUpload = () => {
-    navigate("/upload");
-  };
-
-  return (
-    <div className="header">
-            <div className="header_L">
-                <Link to="/">네이처링_클론</Link>
-            </div>
-            <div className="header_R">
-                <div className="header_R_Top"></div>
-                <div className="header_R_Bottom">
-                    <button onClick={navigateToUpload}>관찰 올리기</button>
-                </div>
-            </div>
-        </div>
-  );
-};
-
-export default Header;
-=======
 import HeaderItem from "./Sections/HeaderItem";
 
 const Header = () => {
@@ -38,4 +10,3 @@ const Header = () => {
 };
 
 export default Header;
->>>>>>> V1
--- server/db.py
+++ server/db.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-# db.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://root:asd6117625@localhost:3306/collection_db?charset=utf8mb4"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-=======
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -21,7 +11,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
->>>>>>> V1
 
 def get_db():
     db = SessionLocal()
@@ -29,9 +18,3 @@ def get_db():
         yield db
     finally:
         db.close()
-<<<<<<< HEAD
-
-
-# DATABASE_URL = "mysql+pymysql://kku:kkukku415@0.0.0.0:3306/collection_db?charset=utf8mb4"
-=======
->>>>>>> V1
